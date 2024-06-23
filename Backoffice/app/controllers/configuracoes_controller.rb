e27e80768cd1f8fa44784edf90983bc16ca56378
Pney <thiagofernandes.123@hotmail.com>
2024-06-23 20:07:16 -0300

    Feat: Integracao do backoffice com o microsservice de emprestimo

--- Backoffice/app/controllers/configuracoes_controller.rb
+++ Backoffice/app/controllers/configuracoes_controller.rb
@@ -9,7 +9,24 @@ class ConfiguracoesController < ApplicationController
   def criar_atualizar_emprestimo
     @configuracao = ConfiguracaoEmprestimo.first_or_initialize
 
-    if @configuracao.update(configuracao_params)
+    if @configuracao.update(emprestimo_params)
+      render json: @configuracao, status: @configuracao.new_record? ? :created : :ok
+    else
+      render json: @configuracao.errors, status: :unprocessable_entity
+    end
+  end
+
+  def investimento
+    @configuracao = ConfiguracaoInvestimento.first
+    return render json: { message: 'Nenhuma configuração encontrada' }, status: 404 if @configuracao.nil?
+
+    render json: @configuracao, status: :ok
+  end
+
+  def criar_atualizar_investimento
+    @configuracao = ConfiguracaoInvestimento.first_or_initialize
+
+    if @configuracao.update(investimento_params)
       render json: @configuracao, status: @configuracao.new_record? ? :created : :ok
     else
       render json: @configuracao.errors, status: :unprocessable_entity
@@ -18,7 +35,11 @@ class ConfiguracoesController < ApplicationController
 
   private
 
-  def configuracao_params
+  def emprestimo_params
     params.permit(:juros, :minimo_parcelamento, :maximo_parcelamento)
+  end
+
+  def investimento_params
+    params.permit(:juros)
   end
 end
--- Backoffice/config/routes.rb
+++ Backoffice/config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
   get '/pegar_emprestimos', to: 'emprestimos#pegar_emprestimos', as: 'pegar_emprestimos'
-  post '/validar_emprestimo', to: 'emprestimos#validar_emprestimo', as: 'validar_emprestimo'
-  
-  get '/configuracao/emprestimo', to: 'configuracoes#emprestimo', as: 'configuracao_emprestimo'
-  post '/configuracao/emprestimo', to: 'configuracoes#criar_atualizar_emprestimo', as: 'configuracao_atualizar_emprestimo'
-
-  # Endpoints de Teste
-  get '/emprestimos', to: 'emprestimos#emprestimos', as: 'emprestimos'
   resources :emprestimos, except: %i[index new create show] do
     member do
       post 'enviar_resultado_validacao'
     end
   end
+  
+  get '/configuracao/emprestimo', to: 'configuracoes#emprestimo', as: 'configuracao_emprestimo'
+  post '/configuracao/emprestimo', to: 'configuracoes#criar_atualizar_emprestimo', as: 'configuracao_atualizar_emprestimo'
+  get '/configuracao/investimento', to: 'configuracoes#investimento', as: 'configuracao_investimento'
+  post '/configuracao/investimento', to: 'configuracoes#criar_atualizar_investimento', as: 'configuracao_atualizar_investimento'
+  
+  # Endpoints de Teste
+  get '/emprestimos', to: 'emprestimos#emprestimos', as: 'emprestimos'
+  post '/validar_emprestimo', to: 'emprestimos#validar_emprestimo', as: 'validar_emprestimo'
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
