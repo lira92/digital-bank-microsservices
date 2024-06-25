feb704d418b3b5d88b94c2f9d1070a4cbcc9e2fc
vinicius <viniciuspicheidt@gmail.com>
2024-06-25 20:32:24 -0300

    build: um monte de coisa

--- Investimento/api/src/main/java/com/investment/api/modules/apiHandler/enums/UrlEnum.java
+++ Investimento/api/src/main/java/com/investment/api/modules/apiHandler/enums/UrlEnum.java
@@ -3,7 +3,7 @@ package com.investment.api.modules.apiHandler.enums;
 public enum UrlEnum {
     CONTA_CORRENTE("http://contacorrente:3003/api/conta"),
     EMAIL("http://notifications-api:3002/notifications"),
-    BACK_OFFICE("http://api:3007/configuracao/investimento");
+    BACK_OFFICE("http://backoffice-api:3007/configuracao/investimento");
 
     private final String value;
 
