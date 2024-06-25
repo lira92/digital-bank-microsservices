2b0e4dd3f64ded1a86759288a614594ffc5b82a9
vinicius <viniciuspicheidt@gmail.com>
2024-06-25 00:51:40 -0300

    feat: add Enum

--- Investimento/api/src/main/java/com/investment/api/modules/moneyTransaction/entities/TransactionHistory.java
+++ Investimento/api/src/main/java/com/investment/api/modules/moneyTransaction/entities/TransactionHistory.java
@@ -9,6 +9,8 @@ import com.investment.api.modules.moneyTransaction.enums.ActionType;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,10 +34,12 @@ public class TransactionHistory {
 
     @NotNull(message = "Tipo de ação não pode estar vazio")
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private ActionType actionType;
 
     @NotNull(message = "Ação não pode estar vazia")
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Action action;
 
     @NotNull(message = "Montante não pode estar vazio")
