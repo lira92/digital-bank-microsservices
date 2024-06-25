d1b264169cc2c2d779ac5b54d6661b07028a7d23
vinicius <viniciuspicheidt@gmail.com>
2024-06-25 11:52:10 -0300

    feat: validated amount

--- Investimento/api/src/main/java/com/investment/api/modules/moneyTransaction/forms/TransactionForm.java
+++ Investimento/api/src/main/java/com/investment/api/modules/moneyTransaction/forms/TransactionForm.java
@@ -2,6 +2,7 @@ package com.investment.api.modules.moneyTransaction.forms;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 
 public record TransactionForm(
@@ -9,5 +10,6 @@ public record TransactionForm(
     @JsonProperty("goal_id")
     Long goalId,
     @NotNull(message = "Valor não pode estar vazio")
+    @DecimalMin(value = "0.01", message = "Valor deve ser maior que 0.01")
     float amount
 ) {}
